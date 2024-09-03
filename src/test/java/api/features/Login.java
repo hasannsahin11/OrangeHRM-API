6565d36407c1c4b41cac04497c8b845df6e3b073
hsahin <qa.hasansahin@gmail.com>
2024-09-02 21:20:13 -0500

    New POM for Logbinder and Data Provider has been added for the negative login test.

--- src/test/java/api/features/Login.java
+++ src/test/java/api/features/Login.java
@@ -2,8 +2,12 @@ package api.features;
 
 
 import api.setup.Setup;
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.http.Cookies;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
@@ -19,20 +23,46 @@ public class Login extends Setup {
     public void essLogin() {
 
         Map<String, String> credentials = new HashMap<>();
-        credentials.put("username", "SHaheen11");
-        credentials.put("password", "SHaheen11");
+        credentials.put("username", "SShaheen11");
+        credentials.put("password", "SShaheen11");
 
         cookies = given()
                 .body(credentials)
-                .spec(requestSpec)
+                .log().all()
+                .contentType(ContentType.JSON)
 
                 .when()
                 .post("/web/index.php/auth/validate")
 
                 .then()
                 .statusCode(302)
-                .spec(responseSpec)
+                .log().all()
                 .contentType(ContentType.HTML)
                 .extract().response().getDetailedCookies();
+    }
+
+    @Test(dataProvider = "loginCredentials")
+    public void essLoginNegative(String username, String password) {
+
+        given()
+                .request().formParam("username", username)
+                .request().formParam("password", password)
+//                .contentType(ContentType.JSON)
+
+                .when()
+                .post("/web/index.php/auth/validate")
+
+                .then()
+                .statusCode(400)
+                .log().all();
+    }
+
+    @DataProvider(name = "loginCredentials")
+    public Object[][] loginData() {
+        return new Object[][]{
+                {"GButlers", "7Sbhaheen7"},   // both invalid username & password
+                {"SShaheen11", "JButlers"},   // valid username, invalid password
+                {"GButlers", "SShaheen11"}    // invalid username, valid password
+        };
     }
 }
--- src/test/java/api/setup/Setup.java
+++ src/test/java/api/setup/Setup.java
@@ -18,18 +18,16 @@ public class Setup {
     public void setup() {
         baseURI = "https://opensource-demo.orangehrmlive.com";
 
-        requestSpec = new RequestSpecBuilder()
-                .log(LogDetail.URI)                      // Prints request uri
-                .log(LogDetail.BODY)                     // Prints request body
-                .setContentType(ContentType.JSON)        // Sets the data format as JSON
-                .build();
+//        requestSpec = new RequestSpecBuilder()
+//                .log(LogDetail.URI)                      // Prints request uri
+//                .log(LogDetail.BODY)                     // Prints request body
+//                .setContentType(ContentType.JSON)        // Sets the data format as JSON
+//                .build();
 
-        responseSpec = new ResponseSpecBuilder()
-//                .expectStatusCode(200)   // Checks if the status code is 200 from all responses
-//                .expectContentType(ContentType.JSON)      // Checks if the response type is in JSON format
-                .log(LogDetail.BODY)                      // prints the body of all responses
-                .build();
-
+//        responseSpec = new ResponseSpecBuilder()
+////                .expectStatusCode(200)   // Checks if the status code is 200 from all responses
+////                .expectContentType(ContentType.JSON)      // Checks if the response type is in JSON format
+//                .log(LogDetail.BODY)                      // prints the body of all responses
+//                .build();
     }
-
 }
